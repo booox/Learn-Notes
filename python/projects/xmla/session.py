ec8278d6d5442ed5239abf02ece27f260352436e
booox <booox@users.noreply.github.com>
2015-12-28 07:12:15 +0800

    Add xmly zhubo class

--- python/projects/xmla/session.py
+++ python/projects/xmla/session.py
@@ -18,15 +18,67 @@ class XMLYSession(requests.Session):
     def __init__(self):
         r = self.get(url=xmly.HOME_URL)
         self._cookies = r.cookies
+
+    def getData(self, url, **kwargs):
+        res = self.get(url=url, **kwargs)
+        self._cookies = res.cookies
+        return res
         
+	def postData(self, url, data=None, **kwargs):
+		data['_xsrf'] = self._cookies['_xsrf']
+		return self.post(url=url, data=data, cookies = self._cookies, **kwargs)
         
     def getZhuboProfile(self, url=None):
         """   Get a zhubo's profile, url is the zhubo's homepage url  """
         
-        if url:
+        # simple check zhubo url
+        url_prefix = xmly.HOME_URL + '/zhubo/'
+        _zbid = url[len(xmly.HOME_URL + '/zhubo/'):].replace('/', '')
+        if not url.startswith(url_prefix) or not _zbid.isdigit():
+            print "[URL ERROR] : Please check the zhubo homepage url."
+            pass
+            exit(0)
+        else:
             zhubo = xmly.Zhubo()    # create a Zhubo instance
             zhubo.url  = url
-            zhubo.zbid = url[len(xmly.HOME_URL + '/zhubo/'):].replace('/', '')
-        else:
+            zhubo.zbid = _zbid
+            
+            res = self.getData(zhubo.url)
+            soup = BeautifulSoup(res.text)
+            
+            try:
+                # balba
+            except:
+                raise
+            
+        
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
             
             
